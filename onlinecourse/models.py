f0b87820cc757fdd504d537683f6e44e5bf56fb6
Liam Callahan <aCodeCrafter@gmail.com>
2022-07-08 15:33:28 +0000

    Add Submission model to models.py,\n fix glitch in Question model

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -104,7 +104,7 @@ class Enrollment(models.Model):
 class Question(models.Model):
     lesson = models.ForeignKey(Lesson,on_delete=models.CASCADE)
     question_text = models.TextField()
-    question_points = models.DecimalField()
+    question_points = models.DecimalField(decimal_places=3,max_digits=6)
 
     def is_get_score(self, selected_ids):
        all_correct_answers = self.choice_set.filter(is_correct=True).count()
@@ -125,7 +125,7 @@ class Choice(models.Model):
     queston = models.ForeignKey(Question,on_delete=models.CASCADE)
     choice_content = models.TextField()
     correct_answer = models.BooleanField()
-    
+
 # <HINT> The submission model
 # One enrollment could have multiple submission
 # One submission could have multiple choices
@@ -134,3 +134,8 @@ class Choice(models.Model):
 #    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
 #    choices = models.ManyToManyField(Choice)
 #    Other fields and methods you would like to design
+
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment,on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
+    
